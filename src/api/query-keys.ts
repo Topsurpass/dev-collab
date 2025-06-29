fabc17e35eae3b9ec78ff66b0e1c4384216d339f
Olowosuyi Temitope <Temz@ETZ-TOLO-AUD.etranzact.com>
2025-06-29 02:53:36 +0100

    Fix query keys type error

--- src/api/query-keys.ts
+++ src/api/query-keys.ts
@@ -1,9 +1,9 @@
 /* eslint-disable no-unused-vars */
-enum QueryKeys {
-	GET_VERIFIED_EMAILS = "GET_VERIFIED_EMAILS",
-	GET_SKILLS = "GET_SKILLS",
-	GET_ROLES = "GET_ROLES",
-	GET_PROFILE = "GET_PROFILE",
-}
+const QueryKeys = {
+	GET_VERIFIED_EMAILS: "GET_VERIFIED_EMAILS",
+	GET_SKILLS: "GET_SKILLS",
+	GET_ROLES: "GET_ROLES",
+	GET_PROFILE: "GET_PROFILE",
+} as const;
 
 export default QueryKeys;
